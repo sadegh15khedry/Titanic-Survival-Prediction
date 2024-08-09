e442f793c01f1731fa25eee9cd7ed822039fe8de
sadegh <sadegh15khedry@gmail.com>
2024-08-09 10:00:11 +0330

    changed the normalizaiton to robust

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -8,3 +8,10 @@ def visualize_correlation_matrix(correlation_matrix):
     plt.title('Correlation Matrix Heatmap')
     plt.savefig('../results/correlation_matrix.png')
     plt.show()
+    
+    
+    
+def get_columns_values_counts(df):
+    for column in df.columns:
+        print(column)
+        print(df[column].value_counts() , '\n')
